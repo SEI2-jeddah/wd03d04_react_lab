ae0c732ce62496b1d7199f7cd25751bdf7b9a8ff
Tareq Alotaibi <tareqdev@protonmail.com>
2019-10-26 19:23:39 +0300

    finished part one REACT PROJECT

---
+++ react-film/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import FilmListing from './FilmListing';
+import FilmDetails from './FilmDetails';
+import './App.css';
+import TMDB from './TMDB'
+
+
+function App() {
+  return (
+  <div className="film-library">
+  <div className="film-list">
+  <FilmListing db={TMDB}/>
+  </div>
+  <div className="film-details">
+  <FilmDetails />
+  </div>
+  </div>
+  );
+}
+
+export default App;
