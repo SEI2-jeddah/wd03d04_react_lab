06942f4279bf0f9e6fa189723bb5c50ad470945c
Majd <majd@Majds-MacBook-Pro.local>
2019-11-12 18:47:59 +0300

    part 1, 2, 3 done

--- react-film/src/App.js
+++ react-film/src/App.js
@@ -1,21 +1,59 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import FilmListing from "./FilmListing";
-import TMND from "./TMDB";
+import React, { Component } from 'react';
+import './App.css';
+import FilmListing from './component/FilmListing';
+import FilmDetails from './component/FilmDetails';
+import TMDB from './TMDB';
+import axios from "axios";
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="film-library">
-        <div className="film-list">
-          <h1 className="section-title">FILMS</h1>
-          <FilmListing films={TMND.films} />
-        </div>
-        <div className="film-details">
-          <h1 className="section-title">DETAILS</h1>
-        </div>
-      </div>
-    );
+
+  constructor(props){
+    super(props)
+    this.state = {
+      films: TMDB.films,
+      faves: [],
+      current: {}
+    } 
+  }
+
+  handleFaveToggle = (film) => {
+
+    const faves = [...this.state.faves];
+    const filmIndex = faves.indexOf(film)
+
+    if (filmIndex !== -1){
+       faves.splice(filmIndex, 1);
+       console.log(`Removing ${film.title} From Favors`)
+    }else{
+      faves.push(film);
+      console.log(`Adding ${film.title} To Favors`)
+    }
+    this.setState({faves})
+  }
+
+  handleDetailsClick = (film) => {
+    
+    const url = `https://api.themoviedb.org/3/movie/${film.id}?api_key=${TMDB.api_key}&append_to_response=videos,images&language=en`
+    
+    axios({
+      method: 'GET',
+      url: url
+    }).then(response => {
+      this.setState({ current: response.data })
+    })
+    .catch(e => {
+      console.log(`There is an Error With axios ${e}`)
+    });  
+}
+
+  render(){
+      return (
+          <div className="film-library">
+              <FilmListing handleDetailsClick={this.handleDetailsClick} films={this.state.films} faves={this.state.faves} onFaveToggle={this.handleFaveToggle}/> 
+              <FilmDetails film={this.state.current} /> 
+          </div>
+      );
   }
 }
 
